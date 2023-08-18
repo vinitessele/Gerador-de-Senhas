81b83ddf4742c2d479f2481d543743a0d2d0d3ab
vinitessele <vinicius_tessele@hotmail.com>
2023-08-18 00:31:51 -0300

    primeiro commit

---
+++ android/app/src/main/kotlin/com/example/gerador_senha/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gerador_senha
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(GeradorSenhasApp());
+}
+
+class GeradorSenhasAppState extends State<GeradorSenhasApp> {
+
+  
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
+
+class GeradorSenhasApp extends StatefulWidget {
+  GeradorSenhasAppState createState() {
+    return GeradorSenhasAppState();
+  }
+}
